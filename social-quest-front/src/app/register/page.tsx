dcc5f19a759e55f4666e590fea649d35a279241a
Simon Frezard <58841798+Mimicmi@users.noreply.github.com>
2024-10-14 14:27:41 +0200

    Feat/login (#1)
    * feat(login): added login page
    
    * feat(login & register): added pages

--- social-quest-front/src/app/register/page.tsx
+++ social-quest-front/src/app/register/page.tsx
@@ -3,21 +3,21 @@
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 
-export default function Home() {
+export default function Register() {
     const router = useRouter();
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
 
     const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault();
-        router.push("/dashboard");
+        router.push("/login");
     };
 
     return (
         <div className="flex items-center justify-center min-h-screen bg-gray-100">
             <div className="p-8 bg-white shadow-md rounded-md w-full max-w-md">
                 <h1 className="text-2xl font-bold mb-6 text-center text-black">
-                    Connectez-vous Social Quest
+                    Inscrivez-vous sur Social Quest
                 </h1>
                 <form onSubmit={handleSubmit} className="space-y-4">
                     <div>
@@ -48,7 +48,7 @@ export default function Home() {
                         type="submit"
                         className="w-full bg-indigo-600 text-white p-2 rounded-md hover:bg-indigo-700"
                     >
-                        Connectez-vous
+                        Inscrivez-vous
                     </button>
                 </form>
             </div>
