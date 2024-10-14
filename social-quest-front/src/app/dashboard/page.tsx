d9d367dfa2d2bd5a1a1f3cce8e901ab883961331
Simon Frezard <simon.frezard@gmail.com>
2024-10-14 14:19:20 +0200

    feat(login): added login page

---
+++ social-quest-front/src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+export default function Dashboard() {
+    return (
+        <div className="flex items-center justify-center min-h-screen bg-gray-100">
+            <h1 className="text-4xl font-bold text-black">
+                Welcome to the Dashboard
+            </h1>
+        </div>
+    );
+}
