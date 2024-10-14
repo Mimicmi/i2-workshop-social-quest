7acbe05a48162b116a7c702a788b5c4432375b74
Simon Frezard <simon.frezard@gmail.com>
2024-10-14 14:26:28 +0200

    feat(login & register): added pages

--- social-quest-front/src/app/dashboard/page.tsx
+++ social-quest-front/src/app/dashboard/page.tsx
@@ -2,7 +2,7 @@ export default function Dashboard() {
     return (
         <div className="flex items-center justify-center min-h-screen bg-gray-100">
             <h1 className="text-4xl font-bold text-black">
-                Welcome to the Dashboard
+                Bienvenue sur le dashboard
             </h1>
         </div>
     );
