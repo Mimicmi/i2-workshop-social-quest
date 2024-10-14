2b23de8ec2252c1c5b8bd96b225af195de9748bf
Simon Frezard <simon.frezard@gmail.com>
2024-10-14 16:01:16 +0200

    Feat(empty templates): added empty templating for project architecture

---
+++ social-quest-front/src/app/chat/page.tsx
@@ -0,0 +1,11 @@
+export default function Chat() {
+    // TODO:  Un chatbot interactif qui guide les utilisateurs confrontés à un problème. Il leur suggère des solutions et
+    // les aide à contacter la bonne personne ou organisation en fonction de la nature du problème
+    // (ex. cyber-harcèlement, signalement de contenu inapproprié).
+
+    return (
+        <>
+            <div>Chat page</div>
+        </>
+    );
+}
---
+++ social-quest-front/src/app/report/page.tsx
@@ -0,0 +1,10 @@
+export default function Report() {
+    // TODO: Vue permettant aux utilisateurs de signaler un comportement ou un contenu inapproprié,
+    // avec un formulaire simple et un système de gestion des alertes.
+
+    return (
+        <>
+            <div>Report page</div>
+        </>
+    );
+}
